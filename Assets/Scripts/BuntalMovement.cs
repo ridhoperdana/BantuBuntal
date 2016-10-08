53ef204122a89c433e71e6e5ad09a786076001b1
cync48 <ridhoperdana@ymail.com>
2016-10-08 16:11:09 +0700

    tambah script

---
+++ Assets/Scripts/BuntalMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuntalMovement : MonoBehaviour {
+
+    public GameObject character;
+    private float speed = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+}
